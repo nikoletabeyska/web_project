3274e5c5897d042cc1090b6cb391b7862d75900f
Mihaela Ilieva <mih1711@abv.bg>
2023-06-20 20:15:06 +0300

    Resolved conflicts and fixed bugs in DB table creation
    New order of dropping and building the DB tables. AUTO_INCREMENTS are all set to 1

--- dump.sql
+++ dump.sql
@@ -3,25 +3,26 @@
 -- Host: localhost    Database: filemeup
 -- ------------------------------------------------------
 -- Server version	10.4.27-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
 -- Table structure for table `files`
---
-
+DROP TABLE IF EXISTS `permissions`;
 DROP TABLE IF EXISTS `files`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+DROP TABLE IF EXISTS `users`;
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL UNIQUE,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+LOCK TABLES `users` WRITE;
+INSERT INTO `users` VALUES (1,'niki','n','b','nik@abv.bg','hi'),(2,'alex','a','z','a@z.bg','bye'),(3,'beyska','beyska','Бейска','nikolet.b@abv.bg','$2y$10$jAKOCubwDnQD9cUOfTod6.UZOvf1W5c6ggmG/W/stzVF9WNLMVGBG'),(4,'nikii','nikoleta','Бейска','alex@abv.bg','$2y$10$Ypj/wT1W.UrFksUDVuqR2.dTJGtC.3tN4geHX.5grsbCSgLXKQhzW');
+UNLOCK TABLES;
+
 CREATE TABLE `files` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -33,26 +34,17 @@ CREATE TABLE `files` (
   PRIMARY KEY (`id`),
   KEY `files_FK_1` (`owner`),
   CONSTRAINT `files_FK_1` FOREIGN KEY (`owner`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=153 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `files`
 --
 
 LOCK TABLES `files` WRITE;
-/*!40000 ALTER TABLE `files` DISABLE KEYS */;
 INSERT INTO `files` VALUES (145,'1.jpg',10811,'image/jpeg','2023-06-20 17:55:36',1,'uploads/1.jpg'),(146,'2.jpg',7785,'image/jpeg','2023-06-20 17:55:36',1,'uploads/2.jpg'),(147,'2-1 (1).webp',59226,'image/webp','2023-06-20 17:55:48',1,'uploads/2-1 (1).webp'),(148,'2-1.webp',59226,'image/webp','2023-06-20 17:55:48',1,'uploads/2-1.webp'),(149,'Екранна снимка 2021-10-23 213258.png',207393,'image/png','2023-06-20 17:56:03',1,'uploads/Екранна снимка 2021-10-23 213258.png'),(150,'Екранна снимка 2021-10-23 214007.png',210563,'image/png','2023-06-20 17:56:03',1,'uploads/Екранна снимка 2021-10-23 214007.png'),(151,'Екранна снимка 2022-10-07 222608.jpg',224315,'image/jpeg','2023-06-20 17:56:03',1,'uploads/Екранна снимка 2022-10-07 222608.jpg'),(152,'Екранна снимка 2022-11-12 194419.jpg',106885,'image/jpeg','2023-06-20 17:56:03',1,'uploads/Екранна снимка 2022-11-12 194419.jpg');
-/*!40000 ALTER TABLE `files` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
 -- Table structure for table `permissions`
---
-
-DROP TABLE IF EXISTS `permissions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `permissions` (
   `permision_id` int(255) NOT NULL AUTO_INCREMENT,
   `file_id` int(255) NOT NULL,
@@ -65,55 +57,12 @@ CREATE TABLE `permissions` (
   CONSTRAINT `permision_FK_1` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE CASCADE,
   CONSTRAINT `permissions_FK_2` FOREIGN KEY (`granted_by`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `permissions_FK_3` FOREIGN KEY (`granted_to`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `permissions`
---
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `permissions` WRITE;
-/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
 -- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'niki','n','b','nik@abv.bg','hi'),(2,'alex','a','z','a@z.bg','bye'),(3,'beyska','beyska','Бейска','nikolet.b@abv.bg','$2y$10$jAKOCubwDnQD9cUOfTod6.UZOvf1W5c6ggmG/W/stzVF9WNLMVGBG'),(4,'nikii','nikoleta','Бейска','alex@abv.bg','$2y$10$Ypj/wT1W.UrFksUDVuqR2.dTJGtC.3tN4geHX.5grsbCSgLXKQhzW');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2023-06-20 18:11:15
---
+++ src/uploads/6490e3f0130162.09135654.md
@@ -0,0 +1,3 @@
+# web_project
+Project for the course of web programming in university. 
+A website for uploading, downloading and sharing files.
