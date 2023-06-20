77182045664e34f6befadbed30684608db8ffb52
Mihaela Ilieva <mih1711@abv.bg>
2023-06-20 20:35:49 +0300

    Get files now work for all users

--- src/getFiles.php
+++ src/getFiles.php
@@ -1,20 +1,19 @@
 <?php
    require_once 'UserRequestHandler.php';
    header('Access-Control-Allow-Origin: *');
-   header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-   header('Access-Control-Allow-Headers: Origin, Content-Type, X-Requested-With, Authorization');
-
-  /* if (isset($_SESSION['user_id'])) {
-      $user_id = $_SESSION['user_id'];
-  } else {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Requested-With, Authorization');
+    session_start();
+  if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $request = new UserRequestHandler();
+    $files = $request->getFiles($user_id);
+    echo json_encode($files);
+  }
+    else {
       // Redirect or handle unauthorized access
       // ...
-  }*/
-
-  $userId = 2;
-  $request = new UserRequestHandler();
-  $files = $request->getFiles($userId);
-  echo json_encode($files);
+    } 
 
 
 
