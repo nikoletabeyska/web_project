76749a713c08754068add4403fa68f9fa6b2d79e
nikoletabeyska <76749430+nikoletabeyska@users.noreply.github.com>
2023-06-21 04:55:29 +0300

    Get files now work for all users

--- src/getFiles.php
+++ src/getFiles.php
@@ -4,20 +4,17 @@
    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Requested-With, Authorization');
 
-  /* if (isset($_SESSION['user_id'])) {
-      $user_id = $_SESSION['user_id'];
-  } else {
+   session_start();
+  if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $request = new UserRequestHandler();
+    $files = $request->getFiles($user_id);
+    echo json_encode($files);
+  }
+    else {
       // Redirect or handle unauthorized access
       // ...
-  }*/
-  $userId = 1;
-  //$userId = $_SESSION['user_id'];
-  $request = new UserRequestHandler();
-  $files = $request->getFiles($userId);
-  echo json_encode($files);
-
-
-
+  }
 
 
 
